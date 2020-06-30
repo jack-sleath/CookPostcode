3e6f8ba9fd350b3a9c40229f062c696707323301
Jack Sleath <jackwilliamsleath@gmail.com>
2020-06-30 21:53:28 +0100

    beginning structure

---
+++ CookPostcode/Interfaces/IPostcodeLookup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookPostcode.Interfaces
+{
+    interface IPostcodeLookup
+    {
+        string[] GetValidDeliveryOptions(string postcode);
+    }
+}
---
+++ CookPostcode/PostcodeData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace CookPostcode
+{
+    public class PostcodeData
+    {
+        public DataSet PostCodeDataSet
+        {
+            get
+            {
+                var dataSet = new DataSet();
+
+                var dataTable = new DataTable();
+
+                dataTable.Columns.Add("Postcode", typeof(string));
+                dataTable.Columns.Add("Delivery_Type", typeof(string));
+
+                dataTable.Rows.Add("TN9", "Delivery from Warehouse");
+                dataTable.Rows.Add("TN9 1AP", "No Deliveries");
+                dataTable.Rows.Add("TN8", "Delivery from Warehouse");
+                dataTable.Rows.Add("TN11", "Delivery from Warehouse");
+                dataTable.Rows.Add("TN1", "Van Delivery, Collect from Tunbridge Wells");
+                dataTable.Rows.Add("TN2", "Van Delivery, Collect from Tunbridge Wells");
+                dataTable.Rows.Add("TN10", "Van Delivery");
+                dataTable.Rows.Add("TN13", "Delivery from Sevenoaks, Collect from Sevenoaks");
+                dataTable.Rows.Add("TN14", "Delivery from Sevenoaks, Collect from Sevenoaks");
+                dataTable.Rows.Add("TN15", "Collect from Sevenoaks");
+                dataTable.Rows.Add("ME", "No Deliveries");
+                dataTable.Rows.Add("ME10", "Collect from Kitchen");
+                dataTable.Rows.Add("ME10 3", "3 Collect from Kitchen, Delivery from Sittingbourne");
+                dataTable.Rows.Add("IV", "No Deliveries");
+
+                dataSet.Tables.Add(dataTable);
+
+                return dataSet;
+            }
+        }
+    }
+}
