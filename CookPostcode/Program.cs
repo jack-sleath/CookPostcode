fa7c83e8e0539e9263779087f265131cca9eaf3f
Jack Sleath <jackwilliamsleath@gmail.com>
2020-06-30 21:06:59 +0100

    Add project files.

---
+++ CookPostcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CookPostcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
