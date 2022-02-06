48a18e68cd8979b08ca68bf32d3dad7a97148229
jacksleath <jackwilliamsleath@gmail.com>
2022-02-06 12:21:00 +0000

    adding unit tests

--- CookPostcodeTests/PostcodeLookupServiceTests.cs
+++ CookPostcodeTests/PostcodeLookupServiceTests.cs
@@ -20,12 +20,42 @@ namespace CookPostcodeTests
         }
 
         [Test]
-        public void NoMatchReturnsExpectedResults() { }
+        public void NoMatchReturnsExpectedResults()
+        {
+            var enteredPostcode = "NOMATCH";
+            postcodeCleanupService.Setup(x => x.CleanPostcode(It.IsAny<string>())).Returns(enteredPostcode);
+            listOfPostcode.Add(new PostcodeDelivery { PostCode = "AAAAAA", Delivery = "Test Message" });
+
+            var result = service.GetValidDeliveryOptions(enteredPostcode, listOfPostcode);
+            Assert.AreEqual("Delivery by Courier", result.DeliveryOption);
+        }
 
         [Test]
-        public void LongestMatchGetsReturned() { }
+        public void LongestMatchGetsReturned()
+        {
+            var enteredPostcode = "AAAAAAB";
+            postcodeCleanupService.Setup(x => x.CleanPostcode(It.IsAny<string>())).Returns(enteredPostcode);
+            listOfPostcode.Add(new PostcodeDelivery { PostCode = "AAAAAA", Delivery = "Six A's" });
+            listOfPostcode.Add(new PostcodeDelivery { PostCode = "AAAAA", Delivery = "Five A's" });
+            listOfPostcode.Add(new PostcodeDelivery { PostCode = "AAAAB", Delivery = "Four A's and a B" });
+
+            var result = service.GetValidDeliveryOptions(enteredPostcode, listOfPostcode);
+            Assert.AreEqual("Six A's", result.DeliveryOption);
+            Assert.AreEqual("AAAAAA", result.Matched);
+        }
 
         [Test]
-        public void MatchesBasedOnStart() { }
+        public void MatchesBasedOnStart()
+        {
+            var enteredPostcode = "STARTHERE";
+            postcodeCleanupService.Setup(x => x.CleanPostcode(It.IsAny<string>())).Returns(enteredPostcode);
+            listOfPostcode.Add(new PostcodeDelivery { PostCode = "START", Delivery = "Postcode begins with START" });
+            listOfPostcode.Add(new PostcodeDelivery { PostCode = "FALSESTART", Delivery = "Postcode begins with FALSE" });
+            listOfPostcode.Add(new PostcodeDelivery { PostCode = "STARTEND", Delivery = "Postcode begins with END" });
+
+            var result = service.GetValidDeliveryOptions(enteredPostcode, listOfPostcode);
+            Assert.AreEqual("Postcode begins with START", result.DeliveryOption);
+            Assert.AreEqual("START", result.Matched);
+        }
     }
 }
